d0cbb2a467ce69bb940f18515d89cbe58616ea0a
Lawrence <theslasher@owner.catalystcraft.tk>
2023-04-25 23:55:46 -0500

    add files for publish

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',  # Define that your audience are developers
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: Apache License 2.0',  # Again, pick a license
+        'License :: OSI Approved :: Apache Software License',  # Again, pick a license
         'Programming Language :: Python :: 3.6',  # Specify which pyhton versions that you want to support
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
